89f22285aaab90202cbede0df790c84315fd76a2
Guillaume Bouvignies <guillaume.bouvignies@ens.psl.eu>
2024-06-23 23:20:13 +0200

    Implement noise estimation and preparation in spectral analysis
    Added functionality to estimate and prepare the noise level for spectral data fitting in noise.py. This includes a new method `estimate_noise` for calculating the noise level based on the standard deviation of the data, and `prepare_noise_level` to set the noise level for fitting processes. Enhancements ensure accurate noise level handling, improving the reliability of spectral analysis results.

--- peakfit/noise.py
+++ peakfit/noise.py
@@ -1,16 +1,26 @@
-from collections.abc import Sequence
+from argparse import Namespace
 
-import lmfit as lf
 import numpy as np
 from lmfit.models import GaussianModel
 from numpy.typing import NDArray
 
-from peakfit.clustering import Cluster
-from peakfit.computing import calculate_shape_heights, residuals, simulate_data
+from peakfit.messages import print_estimated_noise
 from peakfit.spectra import Spectra
 
 FloatArray = NDArray[np.float64]
 IntArray = NDArray[np.int_]
+
+
+def prepare_noise_level(clargs: Namespace, spectra: Spectra) -> float:
+    """Prepare the noise level for fitting."""
+    if clargs.noise is not None and clargs.noise < 0.0:
+        clargs.noise = None
+
+    if clargs.noise is None:
+        clargs.noise = estimate_noise(spectra.data)
+        print_estimated_noise(clargs.noise)
+
+    return clargs.noise
 
 
 def estimate_noise(data: FloatArray) -> float:
@@ -24,86 +34,3 @@ def estimate_noise(data: FloatArray) -> float:
     pars["center"].set(value=0.0, vary=False)
     out = model.fit(y, pars, x=x)
     return out.best_values["sigma"]
-
-
-def extract_noise_spectra(
-    mc: Sequence[str], spectra: Spectra
-) -> tuple[int, FloatArray]:
-    """Extract noise spectra based on monte carlo parameters."""
-    x1, x2, y1, y2, n_iter = mc
-    x1, x2, y1, y2 = (float(val.replace("ppm", "")) for val in (x1, x2, y1, y2))
-    n_iter = int(n_iter)
-    x1_pt, x2_pt = sorted(
-        (spectra.params[2].ppm2pt_i(x1), spectra.params[2].ppm2pt_i(x2))
-    )
-    y1_pt, y2_pt = sorted(
-        (spectra.params[1].ppm2pt_i(y1), spectra.params[1].ppm2pt_i(y2))
-    )
-
-    noise = spectra.data[:, y1_pt:y2_pt, x1_pt:x2_pt]
-    return n_iter, noise
-
-
-def get_some_noise(
-    spectra: Spectra, noise: FloatArray, x_pt: IntArray, y_pt: IntArray
-) -> FloatArray:
-    """Retrieve some noise from spectra data."""
-    rng = np.random.default_rng()
-    rng.shuffle(noise)
-    nz_noise, ny_noise, nx_noise = noise.shape
-
-    x_off, y_off = rng.integers(0, nx_noise - 1), rng.integers(0, ny_noise - 1)
-    x_noise, y_noise = (x_pt + x_off) % nx_noise, (y_pt + y_off) % ny_noise
-
-    return spectra.data[:, y_noise, x_noise].reshape((nz_noise, x_noise.size)).T
-
-
-def calc_err_from_mc(
-    params_list: Sequence[dict], heights_list: FloatArray
-) -> tuple[dict, FloatArray]:
-    """Calculate errors from monte carlo simulations."""
-    params_dict = {}
-
-    for params_mc in params_list:
-        for name, param in params_mc.items():
-            params_dict.setdefault(name, []).append(param.value)
-
-    params_err = {name: np.std(values) for name, values in params_dict.items()}
-    height_err = np.std(heights_list, axis=0, ddof=1)
-
-    return params_err, height_err
-
-
-def monte_carlo(
-    mc: Sequence[str],
-    spectra: Spectra,
-    cluster: Cluster,
-    result: lf.minimizer.MinimizerResult,
-    noise: float,
-) -> tuple[dict, FloatArray]:
-    """Perform monte carlo simulation for error estimation."""
-    n_iter, spectra_noise = extract_noise_spectra(mc, spectra)
-    y_pt, x_pt = cluster.positions
-
-    data_sim = simulate_data(result.params, cluster)
-    mc_list = []
-
-    for _ in range(n_iter):
-        data_noise = get_some_noise(spectra, spectra_noise, x_pt, y_pt)
-        data_mc = data_sim + data_noise
-        cluster_mc = Cluster(cluster.peaks, cluster.positions, data_mc)
-
-        params_mc = lf.minimize(
-            residuals,
-            result.params,
-            args=(cluster_mc, noise),
-            method="least_squares",
-        ).params
-
-        _, heights = calculate_shape_heights(params_mc, cluster_mc)
-        mc_list.append((params_mc, heights))
-
-    params_list, heights_list = zip(*mc_list, strict=False)
-    params_err, height_err = calc_err_from_mc(params_list, heights_list)
-
-    return params_err, height_err
